c208f757134942659d532eec063674eaa5961ecb
Elielton <elieltonramos14@gmail.com>
2024-04-21 22:13:41 -0300

    Criando testes para a pagina principal

--- frontend-bulario/tests/main.test.tsx
+++ frontend-bulario/tests/main.test.tsx
@@ -1,19 +1,91 @@
-import { render } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import Main from '../src/pages/Main';
-import { describe, expect, it } from "vitest";
 import { MemoryRouter } from "react-router-dom";
+import { vi } from 'vitest';
+import userEvent from '@testing-library/user-event';
+import { mockAllMedicaments } from './mocks/mockAllMedicaments';
 
 describe('Testes da página principal', () => {
-  it('Verifica se os testes estão funcionando', () => {
-    expect(true).toBe(true)
-  })
 
-  it('Verifica se a pagina esta sendo renderizada', () => {
+  afterEach(() => {vi.restoreAllMocks()})
+
+  it('Verifica se a pagina esta sendo renderizada', async () => {
+    global.fetch = vi.fn().mockResolvedValue({
+      json: async () => (mockAllMedicaments),
+    });
     render(
       <MemoryRouter>
-        <Main API_URL='http://localhost:3000' itensPage={10} />
-      </MemoryRouter>
-    )
-    expect(document.querySelector('header')).toBeInTheDocument()
+        <Main API_URL='' itensPage={10} />
+      </MemoryRouter>)
+
+    await waitFor(() => {
+      expect(document.querySelector('header')).toBeInTheDocument()
+      expect(document.querySelector('main')).toBeInTheDocument()
+      expect(screen.getByText(/Bulario/i)).toBeInTheDocument()
+      expect(screen.getByText(/Busque um medicamento/i)).toBeInTheDocument()
+      expect(screen.getByText(/Resultados/i)).toBeInTheDocument()
+      expect(screen.getByPlaceholderText(/Digite o nome.../i)).toBeInTheDocument()
+      expect(screen.getByPlaceholderText(/digite o laboratorio.../i)).toBeInTheDocument()
+    });
+  });
+
+  it('Verifica se a listagem de medicamentos está sendo renderizada', async () => {
+    global.fetch = vi.fn().mockResolvedValue({
+      json: () => (mockAllMedicaments),
+    });
+    render(
+      <MemoryRouter>
+        <Main API_URL='' itensPage={10} />
+      </MemoryRouter>)
+
+    await waitFor(() => {
+      expect(document.querySelector('.listMedicaments')).toBeInTheDocument();
+      expect(document.querySelectorAll('.card')).toHaveLength(mockAllMedicaments.length);
+    });
+  });
+
+  it('Verifica se o componente de paginas e exibido', async () => {
+    global.fetch = vi.fn().mockResolvedValue({
+      json: () => (mockAllMedicaments),
+    });
+    render(
+      <MemoryRouter>
+        <Main API_URL='' itensPage={10} />
+      </MemoryRouter>)
+
+    await waitFor(() => {
+      expect(document.querySelector('.pageSelection')).toBeInTheDocument();
+    });
   })
+
+  it('Verifica se a filtragem de medicamentos está funcionando', async () => {
+    global.fetch = vi.fn().mockResolvedValue({
+      json: () => (mockAllMedicaments),
+    });
+    render(
+      <MemoryRouter>
+        <Main API_URL='' itensPage={10} />
+      </MemoryRouter>)
+
+    await waitFor(() => {
+      expect(document.querySelector('.listMedicaments')).toBeInTheDocument();
+      expect(document.querySelectorAll('.card')).toHaveLength(mockAllMedicaments.length);
+    });
+
+    const inputName = screen.getByPlaceholderText(/Digite o nome.../i);
+    await userEvent.type(inputName, 'ACEZO');
+
+    await waitFor(() => {
+      expect(document.querySelectorAll('.card')).toHaveLength(1);
+    });
+
+    inputName.textContent = '';
+
+    const inputCompany = screen.getByPlaceholderText(/digite o laboratorio.../i);
+    await userEvent.type(inputCompany, 'CRISTÁLIA');
+
+    await waitFor(() => {
+      expect(document.querySelectorAll('.card')).toHaveLength(1);
+    });
+  });
 });
---
+++ frontend-bulario/tests/mocks/mockAllMedicaments.ts
@@ -0,0 +1,111 @@
+import { Medicament } from "../../src/types/medicament";
+
+export const mockAllMedicaments: Medicament[] = [
+  {
+  "id": "9fd2789c-50f5-4743-857b-bbfa746ed631",
+  "name": "AMOXICILINA",
+  "published_at": "2022-12-16T18:24:24.000Z",
+  "company": "MULTILAB INDUSTRIA E COMERCIO DE PRODUTOS FARMACEUTICOS LTDA",
+  "documents": [
+    {
+      "id": "57a35a05-1615-491c-b5ae-48ad770cdd53",
+      "expedient": "5064642229",
+      "type": "PROFESSIONAL",
+      "url": "http://localhost:3000/pdf_sample.pdf"
+    },
+    {
+      "id": "dcc3ecc6-5b62-4236-8dfe-f61f4da93fac",
+      "expedient": "5064642229",
+      "type": "PATIENT",
+      "url": "http://localhost:3000/pdf_sample.pdf"
+    }
+  ],
+  "active_principles": [
+    {
+      "id": "595aeb0d-5b0d-4a05-a6f6-574a291ad574",
+      "name": "HIDROQUINONA"
+    }
+  ]
+},
+{
+  "id": "9d0594ea-b9ee-42b6-b8e6-53a43eaf50bf",
+  "name": "ACEZO",
+  "published_at": "2022-11-11T13:52:44.000Z",
+  "company": "CRISTÁLIA PRODUTOS QUÍMICOS FARMACÊUTICOS LTDA.",
+  "documents": [
+    {
+      "id": "5c8c4872-c9c6-4bcd-9667-5014aa602e08",
+      "expedient": "4931014224",
+      "type": "PROFESSIONAL",
+      "url": "http://localhost:3000/pdf_sample.pdf"
+    },
+    {
+      "id": "d2d3ff45-6f5d-463e-ac46-a987f354db98",
+      "expedient": "4931014224",
+      "type": "PATIENT",
+      "url": "http://localhost:3000/pdf_sample.pdf"
+    }
+  ],
+  "active_principles": [
+    {
+      "id": "d0e8fd86-f131-4f9d-aceb-7af4c544e746",
+      "name": "PARACETAMOL"
+    }
+  ]
+},
+{
+  "id": "0566bac0-b5d5-40d7-9eb8-e76c885adf60",
+  "name": "ALPRAZOLAM",
+  "published_at": "2022-07-12T18:15:47.000Z",
+  "company": "EMS SA",
+  "documents": [
+    {
+      "id": "00c1f31e-a561-4f7c-8f61-81ff7a3debad",
+      "expedient": "4415725228",
+      "type": "PATIENT",
+      "url": "http://localhost:3000/pdf_sample.pdf"
+    },
+    {
+      "id": "ba691312-9090-40f0-9368-4718a23a6beb",
+      "expedient": "4415725228",
+      "type": "PROFESSIONAL",
+      "url": "http://localhost:3000/pdf_sample.pdf"
+    }
+  ],
+  "active_principles": [
+    {
+      "id": "674f3f83-a082-483a-b550-e6b8facdb204",
+      "name": "CANDESARTANA CILEXETILA"
+    },
+    {
+      "id": "9ae4d170-c78a-4cea-a7be-fb2bfa7d0433",
+      "name": "INSULINA DETEMIR"
+    }
+  ]
+},
+{
+  "id": "6437d8a8-2694-4d2c-9a10-7163512b3bb7",
+  "name": "AMOXICILINA",
+  "published_at": "2022-08-11T13:32:16.000Z",
+  "company": "ACHÉ LABORATÓRIOS FARMACÊUTICOS S.A",
+  "documents": [
+    {
+      "id": "2a6e87f1-796c-4628-8db5-faf01f9a7eae",
+      "expedient": "3720502210",
+      "type": "PROFESSIONAL",
+      "url": "http://localhost:3000/pdf_sample.pdf"
+    },
+    {
+      "id": "3fd055a8-236c-41ac-87d8-92e0cc96b721",
+      "expedient": "3720502210",
+      "type": "PATIENT",
+      "url": "http://localhost:3000/pdf_sample.pdf"
+    }
+  ],
+  "active_principles": [
+    {
+      "id": "505e6d79-feac-4d79-86d8-7a892ba7a6dc",
+      "name": "ÁCIDO ASCÓRBICO"
+    }
+  ]
+},]
